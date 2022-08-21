426a4e416d462cda9820fd63e1a513f1992b0756
sotasota7 <g19940sm@gm.tsuda.ac.jp>
2022-08-21 21:58:48 +0900

    Changed the HTML and py file for the videoplayback. 0821

--- music_by_feeling/admin.py
+++ music_by_feeling/admin.py
@@ -1,8 +1,9 @@
 # /music_by_feeling/admin.py
 
 from django.contrib import admin
-from .models import Category, Music_by_feeling
+from .models import Category, Music_by_feeling, Comment
 
 
 admin.site.register(Category)
 admin.site.register(Music_by_feeling)
+admin.site.register(Comment)
